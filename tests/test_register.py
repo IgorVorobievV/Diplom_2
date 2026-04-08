e0a354b4ff08de7c1b902df72aaa56c0029d3eda
IgorVorobievV <vorobiev.igor.2012@yandex.ru>
2026-04-08 10:59:44 +0300

    Создал тесты на авторизацию

--- tests/test_register.py
+++ tests/test_register.py
@@ -10,11 +10,9 @@ class TestRegister:
             response = requests.post(REGISTER_URL, data=user_registration["payload"])
         with allure.step("Сохраняем accessToken из ответа."):
             accessToken = response.json().get("accessToken")
-        with allure.step("Сохраняем success из ответа."):
-            success = response.json().get("success")
         with allure.step("Передаем accessToken в фикстуру."):
             user_registration["token"].append(accessToken)
-        assert response.status_code == 200 and success == True
+        assert response.status_code == 200 and '"success":true' in response.text
 
     @allure.title('Создать пользователя, который уже зарегистрирован')
     @allure.description('Проверка статуса 403 и ответ с success: False и message: User alredy exists') 
@@ -31,12 +29,12 @@ class TestRegister:
 
     @allure.title('Создать пользователя и не заполнить одно из обязательных полей')
     @allure.description('Проверка статуса 403 и ответ с success: False и message: Email, password and name are required fields') 
-    @pytest.mark.parametrize('data', ['email', 'password', 'name'])
-    def test_register_incomplete_data_error(self, user_registration, data):
+    @pytest.mark.parametrize('userdata', ['email', 'password', 'name'])
+    def test_register_incomplete_data_error(self, user_registration, userdata):
         with allure.step("Копируем тело запроса из фикстуры."):
             payload = user_registration["payload"].copy()
-        with allure.step(f"Удаляем {payload[data]} из тела запроса."):
-            del payload[data]
+        with allure.step(f"Удаляем {payload[userdata]} из тела запроса."):
+            del payload[userdata]
         with allure.step("Отправляем запрос на создание пользователя и сохраняем ответ в response."):
             response = requests.post(REGISTER_URL, data=payload)
         with allure.step("Сохраняем accessToken из ответа."):
